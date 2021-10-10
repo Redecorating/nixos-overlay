7e99de2a155db617a46036378f7c44c7d1ebdebf
Redecorating <69827514+Redecorating@users.noreply.github.com>
2021-10-11 10:52:36 +1100

    Update overlay.nix

--- overlay.nix
+++ overlay.nix
@@ -3,7 +3,7 @@ let
   callPackage = super.callPackage;
 in
 {
-  apple-bce = callPackage ./packages/kernel-modules/apple-bce { kernel = nixos.linux; };
-  apple-ib-drv = callPackage ./packages/kernel-modules/apple-ib-drv { kernel = nixos.linux; };
+  apple-bce = callPackage ./packages/kernel-modules/apple-bce { };
+  apple-ib-drv = callPackage ./packages/kernel-modules/apple-ib-drv { };
   apple-wifi-firmware = callPackage ./packages/firmware/apple-wifi-firmware { };
 }
