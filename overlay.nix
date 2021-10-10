4646e7e93c4a7af39297cdf49f922e59c466b86c
Redecorating <69827514+Redecorating@users.noreply.github.com>
2021-10-11 10:44:41 +1100

    disable mbp kernel for now

--- overlay.nix
+++ overlay.nix
@@ -3,7 +3,6 @@ let
   callPackage = super.callPackage;
 in
 {
-  linux-mbp = callPackage ./packages/kernel/linux-mbp { };
   apple-bce = callPackage ./packages/kernel-modules/apple-bce { kernel = self.linux-mbp; };
   apple-ib-drv = callPackage ./packages/kernel-modules/apple-ib-drv { kernel = self.linux-mbp; };
   apple-wifi-firmware = callPackage ./packages/firmware/apple-wifi-firmware { };
